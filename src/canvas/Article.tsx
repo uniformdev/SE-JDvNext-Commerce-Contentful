dceab7a87add29d9aa4e2d5cd3007bb9e0ea9d86
John Running <john.a.running@gmail.com>
2023-04-03 07:17:08 -0400

    added enrichments to articles

--- src/canvas/Article.tsx
+++ src/canvas/Article.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useUniformContext } from '@uniformdev/context-react';
 import { ComponentProps, registerUniformComponent } from '@uniformdev/canvas-react';
 import Container, { PaddingSize } from '../components/Container';
 import { withContent } from '../hocs/withContent';
@@ -6,14 +7,22 @@ import { withContent } from '../hocs/withContent';
 type ArticleProps = ComponentProps<{
   title?: string;
   content?: string;
+  unfrmOptEnrichmentTag?: any;
 }>;
 
-const Article: FC<ArticleProps> = ({ title, content = '' }) => (
-  <Container paddingTop={PaddingSize.Large} paddingBottom={PaddingSize.Large}>
-    <h1 className="text-4xl md:text-5xl font-extrabold pb-6 md:pb-14">{title}</h1>
-    {Boolean(content) && <div dangerouslySetInnerHTML={{ __html: content }} className="max-w-5xl prose" />}
-  </Container>
-);
+const Article: FC<ArticleProps> = ({ title, content = '', unfrmOptEnrichmentTag }) => {
+  const { context } = useUniformContext();
+  const enrichments = unfrmOptEnrichmentTag || [];
+  useEffect(() => {
+    context.update({ enrichments });
+  }, [context]);
+  return (
+    <Container paddingTop={PaddingSize.Large} paddingBottom={PaddingSize.Large}>
+      <h1 className="text-4xl md:text-5xl font-extrabold pb-6 md:pb-14">{title}</h1>
+      {Boolean(content) && <div dangerouslySetInnerHTML={{ __html: content }} className="max-w-5xl prose" />}
+    </Container>
+  );
+};
 
 registerUniformComponent({
   type: 'article',
